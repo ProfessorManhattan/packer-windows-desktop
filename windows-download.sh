8b769fbeecaf4cf649f8a9231ab01171b651c716
Brian Zalewski <brian@megabyte.space>
2021-03-30 13:47:13 -0400

    Latest changes

---
+++ windows-download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script should download the latest Windows 10 Enterprise release,
+# rename it to windows.iso in this folder and update the iso_checksum
+# variable in template.json
