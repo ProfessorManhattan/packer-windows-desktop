921981e3e095b8b95258b303f413f794b33998ac
Brian Zalewski <brian@megabyte.space>
2021-03-30 13:52:55 -0400

    Added base file for windows-download.sh.

--- windows-download.sh
+++ windows-download.sh
@@ -2,4 +2,4 @@
 
 # This script should download the latest Windows 10 Enterprise release,
 # rename it to windows.iso in this folder and update the iso_checksum
-# variable in template.json
+# and the iso_version variable in template.json
