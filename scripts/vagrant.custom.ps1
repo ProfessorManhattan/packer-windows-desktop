2edaa2d1562243301f5332ae08c5c563005002ac
Brian Zalewski <brian@megabyte.space>
2021-06-04 19:29:54 -0400

    Latest changes

--- scripts/vagrant.custom.ps1
+++ scripts/vagrant.custom.ps1
@@ -1,2 +1,6 @@
-New-Item -ItemType directory -Path C:\Users\vagrant\.ssh
+try {
+  New-Item -ItemType directory -Path C:\Users\vagrant\.ssh
+} catch {
+  Write-Host "The vagrant .ssh folder already exists"
+}
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub" -OutFile "C:\Users\vagrant\.ssh\authorized_keys"
